f01fc298b1180113d63ef6de72e5f69c727e4842
Slava Baginov <slava-b@yandex-team.ru>
2021-02-20 14:04:13 +0300

    Added CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use quicli::prelude::*;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
 }
